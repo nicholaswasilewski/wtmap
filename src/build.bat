37de1b316fe7045ad2c24006ce908ebd9b680848
nicholaswasilewski <nicholaswasilewski@gmail.com>
2016-02-29 17:47:47 -0500

    compiler and linker flags

--- src/build.bat
+++ src/build.bat
@@ -4,6 +4,6 @@ IF NOT EXIST ..\bin mkdir ..\bin
 pushd ..\bin
 
 rc /fo ..\bin\wtmap.res ..\res\wtmap.rc
-cl /nologo -fp:fast ..\src\win32_wtmap.cpp /Z7 /link wtmap.res /PDB:wtmap.pdb
+cl /nologo /O2 /fp:fast ..\src\win32_wtmap.cpp /Z7 /link wtmap.res /PDB:wtmap.pdb /incremental:no /opt:ref
 
 popd
